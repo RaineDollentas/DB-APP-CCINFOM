5f2d9e151b5170eb8135d7f107f855efabf1ce2d
Tintakol <justin_corpuz@dlsu.edu.ph>
2025-11-19 11:23:31 +0800

    Update ReportsPanel.java

--- src/view/ReportsPanel.java
+++ src/view/ReportsPanel.java
@@ -42,9 +42,6 @@ public class ReportsPanel extends JPanel {
         gbc.gridx = 1; 
         gbc.gridy = 1;
         add(btnDeliveryTrends, gbc);
-
-        // Connect controller
-        new controller.ReportsController(this);
     }
 
     /**
